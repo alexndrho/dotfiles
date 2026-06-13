964fe62fbab8c7f2666b7e8e66f405df3c66731f
Alexander Ho <ho.alexander.g@gmail.com>
2026-06-13 12:28:26 +0800

    refactor(nvim): move which-keys to extras

--- dot_config/nvim/lua/plugins/extra.lua
+++ dot_config/nvim/lua/plugins/extra.lua
@@ -1,5 +1,23 @@
 return {
   {
+    'folke/which-key.nvim',
+    event = 'VimEnter',
+    opts = {
+      delay = 0,
+
+      spec = {
+        { '<leader>b', group = 'Buffer' },
+        { '<leader>g', group = 'Git' },
+        { '<leader>h', group = 'Git Hunk', mode = { 'n', 'v' } },
+        { '<leader>q', group = 'Quit/Session' },
+        { '<leader>s', group = 'Search', mode = { 'n', 'v' } },
+        { '<leader>t', group = 'Toggle' },
+        { 'gr', group = 'LSP Actions', mode = { 'n' } },
+      },
+    },
+  },
+
+  {
     'vyfor/cord.nvim',
     opts = {
       display = {
