8442f035e7bfcfb2fe31b75bca70ee5509bd0a17
Alexander Ho <ho.alexander.g@gmail.com>
2026-07-06 00:28:47 +0800

    fix(nvim): simplify presence configuration

--- home/dot_config/nvim/lua/plugins/misc.lua
+++ home/dot_config/nvim/lua/plugins/misc.lua
@@ -25,16 +25,6 @@ return {
       display = {
         theme = 'minecraft',
       },
-      text = {
-        editing = function(opts) return 'Ruining ' .. opts.filename end,
-        viewing = function(opts) return 'Staring at ' .. opts.filename end,
-        workspace = function(opts) return 'Lost in ' .. opts.workspace end,
-        terminal = 'rm -rf (jk... maybe)',
-        file_browser = 'Lost in the files',
-        plugin_manager = "Another plugin won't hurt",
-        vcs = 'Looking for someone to blame',
-        docs = "Reading docs I won't remember",
-      },
       idle = {
         state = 'Peak procrastination',
         details = 'The cursor blinks. I do not.',
@@ -42,8 +32,8 @@ return {
       },
       buttons = {
         {
-          label = function(opts) return opts.repo_url and 'View Repository' or 'Website' end,
-          url = function(opts) return opts.repo_url or 'https://www.alexanderho.dev/' end,
+          label = 'View Repository',
+          url = function(opts) return opts.repo_url end,
         },
       },
     },
