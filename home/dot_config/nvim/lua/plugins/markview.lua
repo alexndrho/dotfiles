caf7fa03c7f3fdd5835fae92cea78e4813a130e8
Alexander Ho <ho.alexander.g@gmail.com>
2026-07-09 13:22:22 +0800

    fix(nvim): fix markview description

--- home/dot_config/nvim/lua/plugins/markview.lua
+++ home/dot_config/nvim/lua/plugins/markview.lua
@@ -8,8 +8,8 @@ return {
     },
   },
   keys = {
-    { '<leader>tm', '<cmd>Markview toggle<cr>', desc = 'Toggle MarkView' },
+    { '<leader>tm', '<cmd>Markview toggle<cr>', desc = 'Toggle Markview' },
     { '<leader>tM', '<cmd>Markview Toggle<cr>', desc = 'Toggle Markview (global)' },
-    { '<leader>ts', '<cmd>Markview splitToggle<cr>', desc = 'Toggle MarkView (split)' },
+    { '<leader>ts', '<cmd>Markview splitToggle<cr>', desc = 'Toggle Markview (split)' },
   },
 }
