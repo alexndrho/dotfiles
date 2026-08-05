f723b073f55d30a1a2c69a4b2e7aae42b5cdc936
Alexander Ho <ho.alexander.g@gmail.com>
2026-08-05 10:50:36 +0800

    feat(nvim): enable undo even after reopening

--- home/dot_config/nvim/lua/core/options.lua
+++ home/dot_config/nvim/lua/core/options.lua
@@ -8,6 +8,7 @@ vim.o.smartcase = true
 vim.o.signcolumn = 'yes'
 vim.o.showmode = false
 vim.o.cursorline = true
+vim.o.undofile = true
 vim.o.inccommand = 'split'
 vim.o.scrolloff = 10
 vim.o.confirm = true
